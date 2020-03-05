bab93abf69d3f784a3a4088b22b3f9359737f2a5
BOURAKKADI Reda <rbourakk@clio-capgemini.edfgdf.fr>
2020-03-05 17:12:56 +0100

    Ajout du premier tableau

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>REX Stage 2020</title>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-expend sm bg-dark navbar-dark ">
+        <a class="navbar-brand" href="#">Retour d'expérience stage 2020 - STRI </a>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <table class="table table-bordered table-striped ">
+                    <thead class="thead-light">
+                        <tr  class="text-center d-flex" >
+                            <th class="col-sm-8">
+                                Tache
+                            </th>
+                            <th class="col-sm-3">
+                                Date de saisie
+                            </th>
+                            <th class="col-sm-1">                               
+                            </th>
+                        </tr>
+                        <tr class="d-flex">
+                            <td class="col-sm-8">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id temporibus sit debitis eligendi ullam adipisci natus excepturi quae, exercitationem error ut in facilis, voluptatibus saepe commodi cumque ex quia eius.
+                            </td>
+                            <td class="text-center col-sm-3">
+                                jj/mm/aaaa
+                            </td>
+                            <td class="text-center col-sm-1">
+                                <a href="#" class="btn btn-danger"> X </a>                               
+                            </td>
+                        </tr>
+                    </thead>
+                    
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
